9fc905608639506a2f870863c297a46c4ae523a6
Pontus Lidholm <ponli@westermo.com>
2025-09-09 13:48:45 +0200

    add container support

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:lts-alpine
+
+WORKDIR /app
+
+RUN apk update 
+RUN apk add git python3
+RUN git clone https://github.com/strobit/wol-repeater
+WORKDIR /app/wol-repeater
+ENV ENV_ADDR=192.168.1.100
+ENV ENV_BROADCAST=192.168.1.255
+ENV ENV_PORT=9
+CMD ["python3", "wol-repeater.py"]
+
+EXPOSE 9/udp
--- wol-repeater.py
+++ wol-repeater.py
@@ -1,23 +1,25 @@
 import socket
 import sys
+import os
+
 
 def validate_magic(packet):
-    if(len(packet) != 102):
+    if (len(packet) != 102):
         return False
-    
-    #Validate start of packet being ff:ff:ff:ff:ff:ff
+
+    # Validate start of packet being ff:ff:ff:ff:ff:ff
     sum = 0
     for i in range(6):
         sum += packet[i]
 
-    if(sum != 255*6):
+    if (sum != 255 * 6):
         return False
-    
+
     mac = bytearray(6)
     mac = packet[6:12]
-    #Validate 16 instances of the mac address
+    # Validate 16 instances of the mac address
     for i in range(15):
-        if mac != packet[(i+2)*6:(i+3)*6]:
+        if mac != packet[(i + 2) * 6: (i + 3) * 6]:
             return False
     return True
 
@@ -28,8 +30,14 @@ if len(sys.argv) == 3:
     broadcast_ip = sys.argv[2]
     port = 9
 else:
-    print("Run like : python3 server.py <arg1:server ip:this system IP 192.168.1.10> <arg2:broadcast ip:192.168.1.255")
-    exit(1)
+    if "ENV_ADDR" in os.environ and "ENV_BROADCAST" in os.environ and "ENV_PORT" in os.environ:
+        ip = os.environ["ENV_ADDR"]
+        broadcast_ip = os.environ["ENV_BROADCAST"]
+        port = int(os.environ.get("ENV_PORT", 9))
+    else:
+        print("Run like : python3 server.py <arg1:server ip:this system IP 192.168.1.10> <arg2:broadcast ip:192.168.1.255")
+        exit(1)
+
 
 # Create a UDP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -42,15 +50,16 @@ server_address = (ip, port)
 server_socket.bind(server_address)
 
 print("####### wol-relay is listening #######")
-#print("wol-relay listening to:)
-print("IP: %s" % ip)
-print("Port: %s" % port)
+print("Listening IP        : %s" % ip)
+print("Port                : %s" % port)
+print("Broadcast Address   : %s" % broadcast_ip)
 
 while True:
     magic_packet, address = server_socket.recvfrom(4096)
     print("Server received: ", magic_packet, "\n")
     print("Server received len: ", len(magic_packet), "\n")
 
-    if(validate_magic(magic_packet)):
-        print("validated")
+    if (address != broadcast_ip and validate_magic(magic_packet)):
+        print("Packet validated")
+        print("Sending to broadcast address %s" % broadcast_ip)
         server_socket.sendto(magic_packet, (broadcast_ip, port))
